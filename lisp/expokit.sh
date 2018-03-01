3c6bcf48dc053bf3cb8f172ed5f2e7726abd85bb
Peter Karalekas <peter@karalekas.com>
2018-03-01 13:27:07 -0800

    Add expokit installation script to lisp subdirectory

---
+++ lisp/expokit.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+###############################################################################
+# Usage: expokit.sh
+#
+# Install expokit
+#
+# Requires: gfortran, tar, wget
+###############################################################################
+
+# download expokit
+cd /tmp
+wget https://www.maths.uq.edu.au/expokit/expokit.tar.gz
+tar -xf expokit.tar.gz
+rm expokit.tar.gz
+
+# compile shared / dynamic library
+cd /tmp/expokit/fortran
+gfortran -fPIC -c expokit.f
+if [`uname` = "Linux"]
+then
+    gfortran -shared -o expokit.so expokit.o -lblas -L/usr/lib/libblas.so -llapack -L/usr/lib/liblapack.so
+elif [ `uname` = "Darwin" ]
+then
+    gfortran -dynamiclib -o expokit.dylib expokit.o -lblas -llapack
+else
+    echo "Platform `uname` not supported, exiting..."
+fi
+
+# put the library in a usable location
+mv expokit.so /usr/local/lib
+rm -rf /tmp/expokit
