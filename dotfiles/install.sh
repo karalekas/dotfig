1eeb09070b821539a2f819ab6f5a7e4feb2893c7
Peter Karalekas <peter@karalekas.com>
2018-03-01 23:13:39 -0800

    Have dotfiles install script back up existing dotfiles

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -7,11 +7,54 @@
 # Requires: stow
 ################################################################################
 
+DOTFIG_HOME="$HOME/.dotfig"
+DOTFIG_BACKUP="$DOTFIG_HOME/backup/`date "+%F_%T"`"
+
+function back_up_dotfiles() {
+    DIR=$1
+    for DOTFILE in `ls -A $DIR`
+    do
+        if [ -e "$HOME/$DOTFILE" ]
+        then
+            if [ -d "$DOTFIG_BACKUP" ]
+            then
+                echo "Making dotfig backup directory $DOTFIG_BACKUP"
+                mkdir -p $DOTFIG_BACKUP
+            fi
+            echo "Backing up $HOME/$DOTFILE to $DOTFIG_BACKUP/$DOTFILE"
+            if [ -f "$HOME/$DOTFILE" ]
+            then
+                cp $HOME/$DOTFILE $DOTFIG_BACKUP/$DOTFILE
+                rm $HOME/$DOTFILE
+            elif [ -d "$HOME/$DOTFILE" ]
+            then
+                cp -R $HOME/$DOTFILE $DOTFIG_BACKUP/$DOTFILE
+                rm -rf $HOME/$DOTFILE
+            else
+                echo "Unknown file type for $HOME/$DOTFILE, exiting..."
+                exit 1
+            fi
+        fi
+    done
+}
+
+function stow_dotfiles() {
+    DIR=$1
+    echo "Installing $DIR dotfiles..."
+    stow -t $HOME $DIR
+}
+
+if [ ! -d "$DOTFIG_HOME" ]
+then
+    echo "Making dotfig home directory $DOTFIG_HOME"
+    mkdir $DOTFIG_HOME
+fi
+
 for DIR in `ls`
 do
-    if [ -d $DIR ]
+    if [ -d "$DIR" ]
     then
-        echo "Installing $DIR dotfiles..."
-        stow -t $HOME $DIR
+        back_up_dotfiles $DIR
+        stow_dotfiles $DIR
     fi
 done
