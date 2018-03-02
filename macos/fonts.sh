87fce5929e410d6dea6aeee7fdf3a79658b72c2d
Peter Karalekas <peter@karalekas.com>
2018-03-01 21:38:13 -0800

    Have the fonts script clean up after itself

--- macos/fonts.sh
+++ macos/fonts.sh
@@ -7,6 +7,10 @@
 # Requires: tar, wget
 ###############################################################################
 
+pushd /tmp
 wget https://sourceforge.net/projects/dejavu/files/dejavu/2.37/dejavu-fonts-ttf-2.37.tar.bz2
 tar -xvf dejavu-fonts-ttf-2.37.tar.bz2
 cp dejavu-fonts-ttf-2.37/tff/* ~/Library/Fonts
+rm -rf dejavu-fonts-ttf-2.37
+rm dejavu-fonts-ttf-2.37.tar.bz2
+popd
