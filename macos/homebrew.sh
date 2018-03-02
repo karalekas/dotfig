3d6cbc25fee2d10b56ee8dbe383063d39a5f6ff0
Peter Karalekas <peter@karalekas.com>
2018-03-01 16:35:07 -0800

    Add homebrew install script to macos

---
+++ macos/homebrew.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+###############################################################################
+# Usage: homebrew.sh
+#
+# Install Homebrew
+#
+# Requires: xcode-tools (curl, ruby)
+###############################################################################
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
