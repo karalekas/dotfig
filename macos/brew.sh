978626cc34971157310de10801f6aeae080d45c9
Peter Karalekas <peter@karalekas.com>
2018-03-01 16:46:54 -0800

    Add install scripts for brew and cask formulae

---
+++ macos/brew.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+###############################################################################
+# Usage: brew.sh
+#
+# Install Homebrew packages
+#
+# Requires: brew
+###############################################################################
+
+brew install buildapp
+brew install coreutils
+brew install gcc
+brew install htop
+brew install jq
+brew install lapack
+brew install libffi
+brew install postgresql
+brew install rlwrap
+brew install ruby
+brew install sbcl
+brew install stow
+brew install terraform
+brew install tmux
+brew install wget
+brew install xclip
+brew install yarn
+brew install zmq
