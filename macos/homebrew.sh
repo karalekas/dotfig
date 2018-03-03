aef7a3934ddaf4e5cc4a72eca771122d36042454
Peter Karalekas <peter@karalekas.com>
2018-03-02 21:37:02 -0800

    Consolidate homebrew-related scripts, list formulae in text files

--- macos/homebrew.sh
+++ macos/homebrew.sh
@@ -7,4 +7,16 @@
 # Requires: xcode-tools (curl, ruby)
 ###############################################################################
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [ `which brew | echo $?` -ne 0 ]
+then
+    echo "Homebrew is not installed, installing now..."
+    yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "Homebrew is already installed, continuing..."
+fi
+
+echo "Installing brew formulae..."
+cat brew-formulae | xargs brew install
+
+echo "Installing cask formulae..."
+cat cask-formulae | xargs brew cask install
