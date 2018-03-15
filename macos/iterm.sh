5e634365d736e2ce3cebc5be969016e71d942096
Peter Karalekas <peter@karalekas.com>
2018-03-15 12:27:59 -0700

    Check if iTerm2 profile setup is already complete in iterm.sh

--- macos/iterm.sh
+++ macos/iterm.sh
@@ -9,5 +9,18 @@
 # Requires: None
 ###############################################################################
 
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm"
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+if [ `defaults read com.googlecode.iterm2.plist PrefsCustomFolder` = "~/.iterm" ]
+then
+    echo "iTerm2 already configured with a custom preferences folder, continuing..."
+else
+    echo "Setting the iTerm2 custom preferences folder to ~/.iterm..."
+    defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm"
+fi
+
+if [ `defaults read com.googlecode.iterm2.plist LoadPrefsFromCustomFolder` -eq 1 ]
+then
+    echo "iTerm2 already loading preferences from a custom folder, continuing..."
+else
+    echo "Telling iTerm2 to load its preferences from a custom folder..."
+    defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+fi
