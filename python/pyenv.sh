84c63e3cb71a1dd22bb16fade8e79b6af6f24df5
Peter Karalekas <peter@karalekas.com>
2018-03-15 11:45:07 -0700

    Check if pyenv is already installed when running the pyenv script

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -2,7 +2,7 @@
 ###############################################################################
 # Usage: pyenv.sh
 #
-# Install pyenv
+# Install pyenv (takes about 10 minutes)
 #
 # Requires (Mac): curl, readline, xz
 # Requires (Ubuntu): build-essential, curl, libbz2-dev, libncurses5-dev,
@@ -10,8 +10,17 @@
 #   llvm, tk-dev, wget, xz-utils, zlib1g-dev
 ###############################################################################
 
-curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-pyenv install 3.6.3
-pyenv install 2.7.14
-pyenv rehash
-pyenv global 3.6.3 2.7.14
+if [ ! -d "$HOME/.pyenv" ]
+then
+    echo "Pyenv does not exist, installing now... (this takes about 10 minutes)"
+    curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+    echo "Installing Python 3.6.3..."
+    pyenv install 3.6.3
+    echo "Installing Python 2.7.14..."
+    pyenv install 2.7.14
+    pyenv rehash
+    pyenv global 3.6.3 2.7.14
+    echo "Installation complete!"
+else
+    echo "Pyenv already exists, continuing..."
+fi
