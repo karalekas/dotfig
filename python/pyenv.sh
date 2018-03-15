d33c69e27ee9cab6f2ef3a33085b2780ed1f4b3a
Peter Karalekas <peter@karalekas.com>
2018-03-15 00:38:05 -0700

    Update requirements for pyenv.sh and include Ubuntu ones

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -4,7 +4,10 @@
 #
 # Install pyenv
 #
-# Requires: curl
+# Requires (Mac): curl, readline, xz
+# Requires (Ubuntu): build-essential, curl, libbz2-dev, libncurses5-dev,
+#   libncursesw5-dev, libreadline-dev, libsqlite3-dev, libssl-dev
+#   llvm, tk-dev, wget, xz-utils, zlib1g-dev
 ###############################################################################
 
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
