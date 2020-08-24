74ff495a0394b93da8808bc0e1bec4b207c73d87
Peter Karalekas <peter@karalekas.com>
2020-08-23 19:44:27 -0700

    Update python version to 3.8.5, don't install py2

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -2,9 +2,12 @@
 ###############################################################################
 # Usage: pyenv.sh
 #
-# Install pyenv (takes about 10 minutes)
+# Install pyenv (takes about 5 minutes)
 #
 # Requires (Mac): curl, readline, xz
+# Requires (Centos): bzip2-devel, curl, libffi-devel, llvm, lzma,
+#   ncurses-devel, openssl-devel, readline-devel, sqlite-devel, tkinter,
+#   wget, xz, zlib-devel
 # Requires (Ubuntu): build-essential, curl, libbz2-dev, libncurses5-dev,
 #   libncursesw5-dev, libreadline-dev, libsqlite3-dev, libssl-dev
 #   llvm, tk-dev, wget, xz-utils, zlib1g-dev
@@ -12,14 +15,12 @@
 
 if [ ! -d "$HOME/.pyenv" ]
 then
-    echo "Pyenv does not exist, installing now... (this takes about 10 minutes)"
+    echo "Pyenv does not exist, installing now... (this takes about 5 minutes)"
     curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-    echo "Installing Python 3.6.3..."
-    pyenv install 3.6.3
-    echo "Installing Python 2.7.14..."
-    pyenv install 2.7.14
+    echo "Installing Python 3.8.5..."
+    pyenv install 3.8.5
     pyenv rehash
-    pyenv global 3.6.3 2.7.14
+    pyenv global 3.8.5
     echo "Installation complete!"
 else
     echo "Pyenv already exists, continuing..."
